56afc2d64dd710a8279621670e36c832acf368ec
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-11-28 09:33:59 +0600

    addad the main file

---
+++ assets/js/dom.js
@@ -0,0 +1,20 @@
+document.getElementById('login-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    
+    const accountNumber = document.getElementById('account-number').value;
+    
+    const pinNumber =document.getElementById('pin-number').value;
+    const pin = parseInt(pinNumber)
+    if(accountNumber.length === 11){
+        if(pin === 1234){
+            window.location.href="./main.html"
+        }
+        else{
+            alert("Pin Not valid")
+        }
+    }
+    else{
+        alert("Need valid Number")
+    }
+
+})
